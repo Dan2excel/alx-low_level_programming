6961dc082b38262fc3dbea9098a70fd73c085890
Dan2excel <danachonwa@gmail.com>
2023-02-20 16:02:24 -0800

    Positive anything is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main: main function
+ * main: this is the main function
  *
  * Return: always 0
  */
@@ -20,7 +20,7 @@ int main(void)
 		printf("%d is positive", n);
 	else if	(n == 0)
 		printf("%d is zero", n);
-	else
+	else if (n < 0)
 		printf("%d is negative", n);
 	printf("\n");
 	return (0);
