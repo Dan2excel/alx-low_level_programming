8e8c4fbe623e27d1ac166c9c69ae51e7474754b0
Dan2excel <danachonwa@gmail.com>
2023-02-24 13:54:48 -0800

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,13 @@
 #include "main.h"
+
 /**
  * print_alphabet - prints the alphabet in lowercase.
- *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
+
 void print_alphabet(void)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+	print_alphabet();
+	return (0);
 }
 
