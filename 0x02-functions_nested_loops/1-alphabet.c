cf77871442df0fe1321da189a7dd83a90c9232c2
Dan2excel <danachonwa@gmail.com>
2023-02-27 15:42:57 -0800

    Play

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-/**
- * main - prints the alphabet in lowercase.
- * Return: Always 0
- */
+	/**
+	* print_alphabet - print all alphabet in lowercase
+	*/
 
 void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
-}
+	char letter;
 
+	for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+	_putchar('\n');
+}
